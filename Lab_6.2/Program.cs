5554eeecfbbc60d24f5e504c824bfcb01308a120
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2021-12-12 03:12:39 +0300

    Добавьте файлы проекта.

---
+++ Lab_6.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string input = Console.ReadLine();
+           string ss= string.Empty;
+           string sss= string.Empty;   
+            
+            for (int x = input.Length - 1; x >= 0; x--)
+            {
+                if (input[x].ToString() != " ")
+                {
+                    ss += input[x].ToString().ToLower();
+                }
+            }
+            for (int a = 0; a < input.Length; a++)
+            {
+
+                if (input[a].ToString() != " ")
+                {
+                    sss += input[a].ToString().ToLower();
+                }
+            }
+            if (ss == sss)
+            Console.WriteLine("Данное предложение является полидромом");
+            else
+            Console.WriteLine("Данная запись не является полиндромом");
+            Console.ReadKey();
+                    
+            
+        }
+
+        
+    }
+}
+
---
+++ Lab_6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string input = Console.ReadLine();
+            // Представляем строку ввиде массива элементов
+            string[] str = input.Split(new Char[] {' ', ' ', ' ', ' ', ' ',' ', ' '  }, StringSplitOptions.RemoveEmptyEntries); 
+            // Используем метод Split для вывода слова по количеству букв 
+            int maxlen = 0, index = 0;
+            for(int i=0; i<str.Length; i++)
+            {
+                if (str[i].Length > maxlen)
+                {
+                    maxlen = str[i].Length;
+                    index = i;
+                 }
+            }
+            Console.WriteLine("Самое длинное слово: {0}", str[index]);
+            Console.ReadLine(); 
+
+             
+        }
+    }
+}
